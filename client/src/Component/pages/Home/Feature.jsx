e833eeb1de8e8ae4489f97f32500486e634e412c
Ayush Rai <ayushayushravi@gmail.com>
2022-12-11 15:03:39 +0530

    added-card

--- client/src/Component/pages/Home/Feature.jsx
+++ client/src/Component/pages/Home/Feature.jsx
@@ -17,13 +17,11 @@ function Feature() {
     reverse:flip,
     display:500,
     onRest: () => setflip(!flip)
-  
   });
 
   useEffect(()=>{
    const timeout = setTimeout(() => {
-     if( opac.opacity ===  0){
-      
+     if( opac.opacity ===  0){  
       setopac({opacity: 1})
      }
    }, 2000);
@@ -31,15 +29,70 @@ function Feature() {
 
   return (
           <>
-      
-        <div className="middle-container3">
-      
-      <animated.div style={props}>
-      <h1 id="codespeedy"> Dev Home Varanasi</h1>
+     <div className="middle-container3">
+        <animated.div style={props} className='center'>
+      <h1 > Dev Home Varanasi</h1>
       </animated.div>
-       
+      </div>
+      
+      <div className="feature">
+          <div className="feature-title center">
+            <h5 className="">Feature</h5>
+          </div>
+          <div className="feature-body">
 
-    </div>
+            <div className="card1">
+              <div className="card-body">
+                <img src="" alt="" srcset="" />
+                <h5 class="card-title">4BHK Luxious VILLA</h5>
+                <p> Shivpur Varanasi</p>
+                <h4>₹4500 Crore</h4>
+                <div className="list-item">
+                 <div className="item1"><p>4Bhk </p> 
+                     <p> Individual</p><p> House</p></div>
+                    <div className="item2"><p>18050Ft</p></div>
+                    <div className="item3"><p>5</p></div>
+                </div>
+              </div>
+              <button className="card-button" type="button">Click Me!</button>
+              
+              </div>
+            <div className="card2">
+            <div className="card-body">
+                <img src="" alt="" srcset="" />
+                <h5 class="card-title">4BHK Luxious VILLA</h5>
+                <p> Shivpur Varanasi</p>
+                <h4>₹4500 Crore</h4>
+                <div className="list-item">
+                 <div className="item1"><p>4Bhk </p> 
+                     <p> Individual</p><p> House</p></div>
+                    <div className="item2"><p>18050Ft</p></div>
+                    <div className="item3"><p>5</p></div>
+                </div>
+              </div>
+              <button className="card-button" type="button">Click Me!</button>
+            </div>
+            <div className="card3">
+              <div className="card-body">
+                <img src="" alt="" srcset="" />
+                <h5 class="card-title">Parth Residedency</h5>
+                <p>RingRoad Varanasi</p>
+                <h4>₹45 Crore</h4>
+                <div className="list-item">
+                 <div className="item1"><p>4Bhk </p> 
+                     <p> Individual</p><p> House</p></div>
+                    <div className="item2"><p>18050Ft</p></div>
+                    <div className="item3"><p>5</p></div>
+                </div>
+              </div>
+              <button className="card-button" type="button">Click Me!</button>
+              </div>
+          </div>
+
+      </div>
+
+   
+
       
     
     
